8e09216ff299360ff3925a4f4b34b13b1babf8e8
AbdullahShafi1 <120201224+AbdullahShafi1@users.noreply.github.com>
2025-07-08 04:29:20 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azuread_conditional_access_policy" "block_untrusted_access" {
 
     locations {
       included_locations = ["All"]
-      excluded_locations = ["13936a93-d6a4-47c8-b257-ef109483a433"]
+      excluded_locations = [""]
     }
 
     platforms {
